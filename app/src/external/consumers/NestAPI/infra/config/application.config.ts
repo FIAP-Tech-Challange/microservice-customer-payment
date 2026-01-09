9ecaa556c85d12d1a21b90934b20835f0280b897
davidasteixeira <davidasteixeira@gmail.com>
2026-01-09 15:23:37 -0300

    fix: adjusted apikey secret name

--- app/src/external/consumers/NestAPI/infra/config/application.config.ts
+++ app/src/external/consumers/NestAPI/infra/config/application.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   nodeEnv: process.env.NODE_ENV,
-  apiKey: process.env.API_KEY,
+  apiKeySecretName: process.env.API_KEY_SECRET_NAME,
   apiKeyNameOrder: process.env.API_KEY_NAME_ORDER,
   orderServiceBaseUrlParam: process.env.ORDER_SERVICE_BASE_URL_PARAM,
   externalPaymentConsumerKey: process.env.EXTERNAL_PAYMENT_CONSUMER_KEY,
